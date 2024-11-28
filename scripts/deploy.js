749ea27c8860fe9339350674870e82b6f7baf4b9
Justin Bulot <139565161+ZoroTempest@users.noreply.github.com>
2024-11-29 05:24:30 +0800

    Add files via upload

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,19 @@
+const hre = require("hardhat");
 
+async function main() {
+  // No need for initBalance if the constructor doesn't accept it
+  const Assessment = await hre.ethers.getContractFactory("Assessment");
+  
+  // Deploy the contract without any arguments
+  const assessment = await Assessment.deploy();
+  await assessment.deployed();
+
+  console.log(`Contract deployed to ${assessment.address}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
