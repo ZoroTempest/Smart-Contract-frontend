40ab9070b1e2ece250c7861eece2f511ae4f885e
Justin Bulot <139565161+ZoroTempest@users.noreply.github.com>
2024-12-01 23:42:37 +0800

    Add files via upload

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,20 @@
+const hre = require("hardhat");
 
+async function main() {
+  // Step 1: Deploy the contract
+  const Assessment = await hre.ethers.getContractFactory("Assessment");
+  const assessment = await Assessment.deploy();
+  await assessment.deployed();
+
+  console.log(`VotingSystem contract deployed to: ${assessment.address}`);
+
+  // Optionally: Check if candidates were registered by getting candidates count
+  const PlayersCount = await assessment.PlayersCount();
+  console.log(`Total playersregistered: ${PlayersCount}`);
+}
+
+// Run the script
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
